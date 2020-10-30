26002865f345a3a3c7c7570cee1500b2f225c9d2
melisekm <46221266+melisekm@users.noreply.github.com>
2020-10-30 18:25:45 +0100

    tabu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tabu_search
+import timeit
 
 
 def parse_input(path):
@@ -6,10 +7,16 @@ def parse_input(path):
     with open(path, "r") as file:
         for line in file:
             suradnice = line.strip("()\n").split(", ")  # odignoruje () a rozdeli
-            cities.append(suradnice)
+            cities.append(list(map(int, suradnice)))
     return cities
 
 
 if __name__ == "__main__":
     cities = parse_input("vstup.txt")
-    tabu_search.run(cities, 100)
+    start = timeit.default_timer()
+    riesenie = tabu_search.run(cities, 100)
+    end = timeit.default_timer()
+    print(end - start)
+    for mesto in riesenie:
+        print(f"({mesto.x}, {mesto.y})")
+    print(f"cena riesenia: {riesenie.fitness}")
--- tabu_search.py
+++ tabu_search.py
@@ -1,5 +1,6 @@
 import math
 import random
+import timeit
 
 
 class City:
@@ -8,16 +9,21 @@ class City:
         self.y = y
 
 
-class Node:
-    def __init__(self, vector):
-        self.vector = vector
-        self.fitness = calculate_fitness(vector)
+class Node(list):
+    def __init__(self, *args):
+        list.__init__(self, *args)
+        self.fitness = self.calculate_fitness()
 
-    def calculate_fitness(vector):
-        pass
+    def calculate_fitness(self):
+        result = 0
+        for i in range(len(self) - 1):
+            result += euclidian_d(self[i], self[i + 1])
+        result += euclidian_d(self[-1], self[0])
+        return result
 
-    def euclidian_d(city_A, city_B):
-        return math.sqrt((city_A.x - city_B.x) ** 2 + (city_A.y - city_B.y) ** 2)
+
+def euclidian_d(city_A, city_B):
+    return math.sqrt((city_A.x - city_B.x) ** 2 + (city_A.y - city_B.y) ** 2)
 
 
 def vygenerujNasledovnikov(parent):
@@ -27,19 +33,43 @@ def vygenerujNasledovnikov(parent):
         for j in range(i + 1, len(parent)):
             nasledovnik = parent.copy()
             nasledovnik[i], nasledovnik[j] = nasledovnik[j], nasledovnik[i]
-            nasledovnici.append(nasledovnik)
+
+            nasledovnici.append(Node(nasledovnik))
 
     return nasledovnici
 
 
+def najdi_najlepsieho_kandidata(nasledovnici, tabu_list):
+    kandidat = nasledovnici[0]
+
+    for nasledovnik in nasledovnici:
+        if not nasledovnik in tabu_list and nasledovnik.fitness < kandidat.fitness:
+            kandidat = nasledovnik
+
+    return kandidat
+
+
+def stop(start):
+    return timeit.default_timer() - start > 0.5
+
+
 def run(cities, maxTabuSize):
-    riesenie = None
     maxTabuSize = 100
-    nahodny = [City(city[0], city[1]) for city in cities]
+    nahodny = Node([City(city[0], city[1]) for city in cities])
     globalne_max = nahodny
     najlepsi_kandidat = nahodny
-    tabuList = []
-    tabuList.append(nahodny)
-
-    while True:
+    tabu_list = []
+    tabu_list.append(nahodny)
+    start = timeit.default_timer()
+    while not stop(start):
         nasledovnici = vygenerujNasledovnikov(najlepsi_kandidat)
+        najlepsi_kandidat = najdi_najlepsieho_kandidata(nasledovnici, tabu_list)
+
+        if najlepsi_kandidat.fitness < globalne_max.fitness:
+            globalne_max = najlepsi_kandidat
+
+        tabu_list.append(najlepsi_kandidat)
+        if len(tabu_list) > maxTabuSize:
+            tabu_list.pop()
+
+    return globalne_max
